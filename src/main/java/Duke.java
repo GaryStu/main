c8d31e5dffcaccaf0e9c3c975ba36cf73d8ad33d
Ivan Andika <ivanandika@u.nus.edu>
2019-08-22 23:26:03 +0800

    Add branches and finish level 1.0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,41 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        //Greeting the user
+        String boundary = "    ____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(boundary);
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println(boundary);
+        System.out.println();
+        echo();
     }
+
+    public static void echo() {
+        String boundary = "    ____________________________________________________________";
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(boundary);
+                System.out.println("     " + "Bye. Hope to see you again soon!");
+                System.out.println(boundary);
+                System.out.println();
+                break;
+            }
+            else {
+                System.out.println(boundary);
+                System.out.println("     " + input);
+                System.out.println(boundary);
+                System.out.println();
+            }
+        }
+
+
+    }
+
 }
+
+
